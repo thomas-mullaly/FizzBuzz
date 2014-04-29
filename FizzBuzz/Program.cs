7d64a0110742bffe2cab2da7697b06fb5c35becc
Thomas Mullaly <tmullaly88@gmail.com>
2014-04-29 15:06:31 -0400

    Added current implementation of the FizzBuzz library

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FizzBuzzLib;
+
+namespace FizzBuzzApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            for (int i = 1; i <= 100; ++i)
+            {
+                Console.WriteLine(fizzBuzz.ProcessNumber(i));
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ FizzBuzzLib/FizzBuzz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzLib
+{
+    public class FizzBuzz
+    {
+        public string ProcessNumber(int number)
+        {
+            string processedNumber = String.Empty;
+
+            if (number % 3 == 0)
+            {
+                processedNumber = "Fizz";
+            }
+
+            if (number % 5 == 0)
+            {
+                processedNumber += "Buzz";
+            }
+
+            if (!String.IsNullOrEmpty(processedNumber))
+            {
+                return processedNumber;
+            }
+
+            return number.ToString();
+        }
+    }
+}
